2a564efa06ef57131c63b9995a43d6c8973f8189
NoSacred <und3rrrr@gmail.com>
2020-02-13 17:56:30 +0300

    homework2finish

--- AndroidStudioProjects/homeWork2/app/src/main/java/homework2/skillboxhelloworld/com/MainActivity.kt
+++ AndroidStudioProjects/homeWork2/app/src/main/java/homework2/skillboxhelloworld/com/MainActivity.kt
@@ -1,12 +1,23 @@
 package homework2.skillboxhelloworld.com
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text ="""
+            BuildType = ${BuildConfig.BUILD_TYPE}
+            Flavor = ${BuildConfig.FLAVOR}
+            VersionCode = ${BuildConfig.VERSION_CODE}
+            VersionName = ${BuildConfig.VERSION_NAME}
+            ApplicationId = ${BuildConfig.APPLICATION_ID}
+            
+        """.trimIndent()
     }
 }
