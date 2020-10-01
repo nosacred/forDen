86007e2f722b1334882f9f115107219083f09f0f
NoSacred <und3rrrr@gmail.com>
2020-10-01 15:40:50 +0300

    6.1FULL

--- IdeaProjects/JavaLessons/src/workpack/BankAccount.java
+++ IdeaProjects/JavaLessons/src/workpack/BankAccount.java
@@ -7,12 +7,12 @@ public class BankAccount {
     }
 
     public void putMoney(double amount){
-        balance+= amount;
+        balance+= Math.abs(amount);
     }
 
     public void takeMoney(double amount){
         if(balance >= amount){
-            balance-=amount;
+            balance-=Math.abs(amount);
         }
     }
 
@@ -24,7 +24,7 @@ public class BankAccount {
         if (balance >= amount){
             takeMoney(amount);
             client.putMoney(amount);
-            System.out.println("Произошло списание денежных средств в размере: "+ amount);
+            System.out.println("Произошло списание денежных средств в размере: "+ Math.abs(amount));
             return true;
         }
         else System.out.println(" На вашем счете недостаточно средств для совершения данной операции! Пополните баланс");
